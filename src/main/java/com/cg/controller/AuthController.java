72140180d56154f883e83c01cb8828fcc7eddc5c
riya-pawar18 <riyapawar941@gmail.com>
2026-04-21 11:21:19 +0530

    auth modified

--- src/main/java/com/cg/controller/AuthController.java
+++ src/main/java/com/cg/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.cg.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,5 +46,12 @@ public class AuthController
 		response.addHeader("Set-Cookie", cookie);
 		return new ResponseEntity<>(res,HttpStatus.OK);
 	}
+	@GetMapping("/me")
+    public ResponseEntity<?> me(Authentication auth) {
+        if (auth == null || !auth.isAuthenticated()) {
+            return ResponseEntity.status(401).build();
+        }
+        return ResponseEntity.ok(auth.getName()); 
+    }
 	
 }
